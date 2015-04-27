d3d0926edbc8d74f1315899bb3c2e84db30fd77c
Philipp Möller <bootsarehax@gmail.com>
2015-04-27 10:25:03 +0200

    Addd a skeleton entrypoint for the containers

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script is the entrypoint of a CGAL testsuite docker container.
+set -e
+
+CGAL_DIR='/mnt/testsuite/'
+CGAL_SRC_DIR='/mnt/testsuite/src/'
+CGAL_TEST_DIR='/mnt/testsuite/test/'
+
+# Build CGAL
+
+# Build and Execute the Tests
+
+# Build and Execute the Examples
