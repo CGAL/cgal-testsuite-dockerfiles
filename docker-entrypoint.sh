3c8a4ea0d72b4c0d5d675febb32401bd5c281420
Philipp Möller <bootsarehax@gmail.com>
2015-04-27 12:54:49 +0200

    Split the binary directories into src and test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,15 +10,25 @@ CGAL_TEST_DIR='/mnt/testsuite/test/'
 
 CGAL_LOG_FILE="$CGAL_TESTRESULTS${1}.log"
 
+# Create the binary directories
 if [ ! -d "$CGAL_BINARY_DIR" ]; then 
     mkdir "$CGAL_BINARY_DIR"
 fi
+if [ ! -d "$CGAL_BINARY_DIR/src" ]; then
+    mkdir "$CGAL_BINARY_DIR/src"
+fi
+if [ ! -d "$CGAL_BINARY_DIR/test" ]; then
+    mkdir "$CGAL_BINARY_DIR/test"
+fi
+
 
 # Build CGAL
-cd "$CGAL_BINARY_DIR"
+cd "$CGAL_BINARY_DIR/src"
 cmake -DRUNNING_CGAL_AUTO_TEST=TRUE VERBOSE=1 "$CGAL_DIR" >> "$CGAL_LOG_FILE" >&1
 make VERBOSE=ON -k -fMakefile >> "$CGAL_LOG_FILE"
 
 # Build and Execute the Tests
+cd "$CGAL_BINARY_DIR/test"
+
 
 # Build and Execute the Examples
