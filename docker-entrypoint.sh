7d41ee1cb985816700b1e2e1181986fdade58bf1
Philipp Möller <bootsarehax@gmail.com>
2015-06-17 10:08:54 +0200

    Create cgal_tester user/group with uid/gid taken from /mnt/testresults
    /mnt/testresults inherits permissions from the host and changing
    permissions propagates to the host, leaving it with a possibly
    non-existant uid/gid and preventing test_cgal from accessing the
    results. To prevent this the cgal_tester user is created with matching
    uid/gid.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,13 @@
 # This script is the entrypoint of a CGAL testsuite docker container.
 set -e
 
+testresult_owner_uid=$(stat -c '%u' /mnt/testresults)
+testresult_group_id=$(stat -c '%g' /mnt/testresults)
+
+
 # Only add cgal_tester group and user, when they do not already exist.
-groupadd -f cgal_tester
-id -u cgal_tester &>/dev/null || useradd -d /home/cgal_tester -g cgal_tester cgal_tester
+groupadd -o -g $testresult_group_id -f cgal_tester
+id -u cgal_tester &>/dev/null || useradd -o -u $testresult_owner_uid -d /home/cgal_tester -g cgal_tester cgal_tester
 # useradd will not create the home directory and we cannot use
 # adduser, because it is Debian specific
 mkdir -p /home/cgal_tester
