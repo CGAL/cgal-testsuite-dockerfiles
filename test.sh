e9472b21b34b86565b1b8e749e28a547935d27e2
Maxime Gimeno <maxime.gimeno@gmail.com>
2016-11-23 17:18:32 +0100

    WIP

--- test.sh
+++ test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash 
 
-if [ "$1" == --Fedora ]
+if [ $1 ='ArchLinux' ]
 then
   docker build -t cgal/testsuite-docker:fedora ./Fedora
+elif [ $1 ='CentOS-5' ]
+then
+elif [ $1 ='CentOS-6' ]
+then
+elif [ $1 ='CentOS-7' ]
+then
+elif [ $1 ='Debian' ]
+then
+elif [ $1 ='Fedora' ]
+then
+elif [ $1 ='Ubuntu' ]
+then
 else
   return 1;
 fi
+
