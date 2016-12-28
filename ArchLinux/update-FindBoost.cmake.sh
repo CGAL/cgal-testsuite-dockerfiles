cf02e3b3541bf1668337ae45221ebd9a427f7ace
Laurent Rineau <laurent.rineau@cgal.org>
2016-12-28 14:13:42 +0100

    Fix the script to patch Boost

--- ArchLinux/update-FindBoost.cmake.sh
+++ ArchLinux/update-FindBoost.cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 case $(pacman -Q cmake) in
-    "cmake 3.6.3"*) cd /usr/share/cmake-3.6/Modules; [ -e FindBoost.cmake.orig ] || patch -p2 < <(cat /updates/*.patch);;
+    "cmake 3.6.3"*) [ -e /usr/share/cmake-3.6/Modules/FindBoost.cmake.orig ] || (cd /usr/share/cmake-3.6/Modules && patch -p2 < <(cat /updates/*.patch) );;
 esac
