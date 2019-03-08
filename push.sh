0551723c1767a01925d72d0640bdd66c128c59b2
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-03-08 09:28:57 +0100

    Add push and rename platform

--- push.sh
+++ push.sh
@@ -35,6 +35,8 @@ elif [ "$1" = Debian-stable ]
 then
    docker push cgal/testsuite-docker:debian-stable
    docker push cgal/testsuite-docker:debian-stable-release
+elif [ "$1" = Debian-stable-cross-compilation-for-arm ];then
+  docker push cgal/testsuite-docker:debian-stable-cross-compilation-for-arm
 elif [ "$1" = Debian-testing ]
 then
    docker push cgal/testsuite-docker:debian-testing
