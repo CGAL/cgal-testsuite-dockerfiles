f145a7fe0d2d56e9fcee7179e3580d8e871c04c9
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-04-30 11:51:32 +0200

    Update script name in startup.sh

--- Debian-stable-cross-compilation-for-arm/startup.sh
+++ Debian-stable-cross-compilation-for-arm/startup.sh
@@ -8,7 +8,7 @@ cp /tmp_ssh/* ~/.ssh
 cd /cgal_root/
 source "env.sh"
 #launch script
-bash $CGAL_DIR/${SCRIPTS_DIR}developer_scripts/run_testsuite_with_cmake
+bash $CGAL_DIR/${SCRIPTS_DIR}developer_scripts/run_testsuite_with_ctest
 #clean-up
 cd /cgal_root/
 rm env.sh
