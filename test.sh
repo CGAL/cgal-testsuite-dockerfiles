44c18c420fe85f3dff54a9871a8359d8f16a1a0f
Laurent Rineau <laurent.rineau@cgal.org>
2022-04-27 15:30:43 +0200

    Add a DOCKERFILE_URL env variable to Docker images built by Github Actions

--- test.sh
+++ test.sh
@@ -2,26 +2,38 @@
 
 set -e
 
+if [ -n "$GITHUB_SHA" ]; then
+  COMMIT_URL=https://github.com/${GITHUB_REPOSITORY}/blob/${GITHUB_SHA}
+fi
+
+function dockerbuild() {
+  if [ -z "$GITHUB_SHA" ]; then
+    docker build -t cgal/testsuite-docker:$1 ./$2
+  else
+    docker build --build-arg dockerfile_url=${COMMIT_URL}/$2/Dockerfile -t cgal/testsuite-docker:$1 ./$2
+  fi
+}
+
 if [ "$1" = ArchLinux ]
 then
-  docker build -t cgal/testsuite-docker:archlinux ./ArchLinux
-  docker build -t cgal/testsuite-docker:archlinux-cxx14 ./ArchLinux-CXX14
-  docker build -t cgal/testsuite-docker:archlinux-cxx17-release ./ArchLinux-CXX17-Release
-  docker build -t cgal/testsuite-docker:archlinux-clang ./ArchLinux-clang
-  docker build -t cgal/testsuite-docker:archlinux-clang-cxx14 ./ArchLinux-clang-CXX14
-  docker build -t cgal/testsuite-docker:archlinux-clang-cxx17-release ./ArchLinux-clang-CXX17-Release
-  docker build -t cgal/testsuite-docker:archlinux-clang-cxx20-release ./ArchLinux-clang-CXX20-Release
-  docker build -t cgal/testsuite-docker:archlinux-clang-release ./ArchLinux-clang-Release
+  dockerbuild archlinux ArchLinux
+  dockerbuild archlinux-cxx14 ArchLinux-CXX14
+  dockerbuild archlinux-cxx17-release ArchLinux-CXX17-Release
+  dockerbuild archlinux-clang ArchLinux-clang
+  dockerbuild archlinux-clang-cxx14 ArchLinux-clang-CXX14
+  dockerbuild archlinux-clang-cxx17-release ArchLinux-clang-CXX17-Release
+  dockerbuild archlinux-clang-cxx20-release ArchLinux-clang-CXX20-Release
+  dockerbuild archlinux-clang-release ArchLinux-clang-Release
 elif [ "$1" = CentOS-5 ]
 then
-  docker build -t cgal/testsuite-docker:centos5 ./CentOS-5
+  dockerbuild centos5 CentOS-5
 elif [ "$1" = CentOS-6 ]
 then
-  docker build -t cgal/testsuite-docker:centos6 ./CentOS-6
-  docker build -t cgal/testsuite-docker:centos6-cxx11-boost157 ./CentOS-6-CXX11-Boost157
+  dockerbuild centos6 CentOS-6
+  dockerbuild centos6-cxx11-boost157 CentOS-6-CXX11-Boost157
 elif [ "$1" = CentOS-6-32 ]
 then
-  docker build -t cgal/testsuite-docker:centos6-32 ./CentOS-6-32
+  dockerbuild centos6-32 CentOS-6-32
 elif [ "$1" = CentOS-7-ICC-beta ]
 then
     if [ -z "$ICC_BETA_ACTIVATION_SERIAL_NUMBER" -a -n "$TRAVIS_PULL_REQUEST" ]; then
@@ -37,44 +49,44 @@ then
     fi
 elif [ "$1" = CentOS-7-ICC ]
 then
-  docker build -t cgal/testsuite-docker:centos7-icc ./CentOS-7-ICC
+  dockerbuild centos7-icc CentOS-7-ICC
 elif [ "$1" = CentOS-7 ]
 then
-  docker build -t cgal/testsuite-docker:centos7 ./CentOS-7
-  docker build -t cgal/testsuite-docker:centos7-release ./CentOS-7-Release
+  dockerbuild centos7 CentOS-7
+  dockerbuild centos7-release CentOS-7-Release
 elif [ "$1" = Debian-stable ]
 then
-  docker build -t cgal/testsuite-docker:debian-stable ./Debian-stable
-  docker build -t cgal/testsuite-docker:debian-stable-release ./Debian-stable-Release
-  docker build -t cgal/testsuite-docker:debian-stable-cross-compilation-for-arm ./Debian-stable-cross-compilation-for-arm
+  dockerbuild debian-stable Debian-stable
+  dockerbuild debian-stable-release Debian-stable-Release
+  dockerbuild debian-stable-cross-compilation-for-arm Debian-stable-cross-compilation-for-arm
 elif [ "$1" = Debian-testing ]
 then
-  docker build -t cgal/testsuite-docker:debian-testing ./Debian-testing
+  dockerbuild debian-testing Debian-testing
 elif [ "$1" = Fedora ]
 then
-  docker build -t cgal/testsuite-docker:fedora ./Fedora
-  docker build -t cgal/testsuite-docker:fedora-with-leda ./Fedora-with-LEDA
-  docker build -t cgal/testsuite-docker:fedora-release ./Fedora-Release
-  docker build -t cgal/testsuite-docker:fedora-strict-ansi ./Fedora-strict-ansi
+  dockerbuild fedora Fedora
+  dockerbuild fedora-with-leda Fedora-with-LEDA
+  dockerbuild fedora-release Fedora-Release
+  dockerbuild fedora-strict-ansi Fedora-strict-ansi
 elif [ "$1" = Fedora-32 ]
 then
-  docker build -t cgal/testsuite-docker:fedora-32 ./Fedora-32
-  docker build -t cgal/testsuite-docker:fedora-32-release ./Fedora-32-Release
+  dockerbuild fedora-32 Fedora-32
+  dockerbuild fedora-32-release Fedora-32-Release
 elif [ "$1" = Fedora-rawhide ]
 then
-  docker build -t cgal/testsuite-docker:fedora-rawhide ./Fedora-rawhide
-  docker build -t cgal/testsuite-docker:fedora-rawhide-release ./Fedora-rawhide-Release
+  dockerbuild fedora-rawhide Fedora-rawhide
+  dockerbuild fedora-rawhide-release Fedora-rawhide-Release
 elif [ "$1" = Ubuntu ]
 then
-  docker build -t cgal/testsuite-docker:ubuntu ./Ubuntu
-  docker build -t cgal/testsuite-docker:ubuntu-cxx11 ./Ubuntu-CXX11
-  docker build -t cgal/testsuite-docker:ubuntu-no-deprecated-code ./Ubuntu-NO_DEPRECATED_CODE
-  docker build -t cgal/testsuite-docker:ubuntu-no-gmp-no-leda ./Ubuntu-no-gmp-no-leda
+  dockerbuild ubuntu Ubuntu
+  dockerbuild ubuntu-cxx11 Ubuntu-CXX11
+  dockerbuild ubuntu-no-deprecated-code Ubuntu-NO_DEPRECATED_CODE
+  dockerbuild ubuntu-no-gmp-no-leda Ubuntu-no-gmp-no-leda
 elif [ "$1" = Ubuntu-GCC-master ]
 then
-  docker build -t cgal/testsuite-docker:ubuntu-gcc6 ./Ubuntu-GCC6
-  docker build -t cgal/testsuite-docker:ubuntu-gcc6-cxx1z ./Ubuntu-GCC6-CXX1Z
-  docker build -t cgal/testsuite-docker:ubuntu-gcc6-release ./Ubuntu-GCC6-Release
-  docker build -t cgal/testsuite-docker:ubuntu-gcc_master_cxx20-release ./Ubuntu-GCC_master_cpp20-Release
+  dockerbuild ubuntu-gcc6 Ubuntu-GCC6
+  dockerbuild ubuntu-gcc6-cxx1z Ubuntu-GCC6-CXX1Z
+  dockerbuild ubuntu-gcc6-release Ubuntu-GCC6-Release
+  dockerbuild ubuntu-gcc_master_cxx20-release Ubuntu-GCC_master_cpp20-Release
 fi
 docker images
