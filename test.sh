a89f0f10ade403d6115de409b8a789ed1eaf7d9b
Laurent Rineau <laurent.rineau@cgal.org>
2023-07-19 14:50:16 +0200

    test and fix images

--- test.sh
+++ test.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+curl -o cgal.tar.gz  -L $(curl -s https://api.github.com/repos/CGAL/cgal/releases/latest | jq -r .tarball_url)
+mkdir -p cgal
+tar -xzf cgal.tar.gz -C cgal --strip-components=1
+
+if command -v selinuxenabled >/dev/null && selinuxenabled; then
+  chcon -Rt container_file_t cgal
+fi
+
 if [ -n "$GITHUB_SHA" ]; then
   COMMIT_URL=https://github.com/${GITHUB_REPOSITORY}/blob/${GITHUB_SHA}
 fi
@@ -14,26 +22,31 @@ function dockerbuild() {
   fi
 }
 
+function dockerbuildandtest() {
+  dockerbuild $1 $2
+  docker run --rm -v $PWD/cgal:/cgal cgal/testsuite-docker:$1 bash -c 'cmake -DWITH_examples=ON /cgal && cmake --build . -t terrain'
+}
+
 if [ "$1" = ArchLinux ]
 then
-  dockerbuild archlinux ArchLinux
-  dockerbuild archlinux-cxx14 ArchLinux-CXX14
-  dockerbuild archlinux-cxx17-release ArchLinux-CXX17-Release
-  dockerbuild archlinux-clang ArchLinux-clang
-  dockerbuild archlinux-clang-cxx14 ArchLinux-clang-CXX14
-  dockerbuild archlinux-clang-cxx17-release ArchLinux-clang-CXX17-Release
-  dockerbuild archlinux-clang-cxx20-release ArchLinux-clang-CXX20-Release
-  dockerbuild archlinux-clang-release ArchLinux-clang-Release
+  dockerbuildandtest archlinux ArchLinux
+  dockerbuildandtest archlinux-cxx14 ArchLinux-CXX14
+  dockerbuildandtest archlinux-cxx17-release ArchLinux-CXX17-Release
+  dockerbuildandtest archlinux-clang ArchLinux-clang
+  dockerbuildandtest archlinux-clang-cxx14 ArchLinux-clang-CXX14
+  dockerbuildandtest archlinux-clang-cxx17-release ArchLinux-clang-CXX17-Release
+  dockerbuildandtest archlinux-clang-cxx20-release ArchLinux-clang-CXX20-Release
+  dockerbuildandtest archlinux-clang-release ArchLinux-clang-Release
 elif [ "$1" = CentOS-5 ]
 then
-  dockerbuild centos5 CentOS-5
+  dockerbuildandtest centos5 CentOS-5
 elif [ "$1" = CentOS-6 ]
 then
-  dockerbuild centos6 CentOS-6
-  dockerbuild centos6-cxx11-boost157 CentOS-6-CXX11-Boost157
+  dockerbuildandtest centos6 CentOS-6
+  dockerbuildandtest centos6-cxx11-boost157 CentOS-6-CXX11-Boost157
 elif [ "$1" = CentOS-6-32 ]
 then
-  dockerbuild centos6-32 CentOS-6-32
+  dockerbuildandtest centos6-32 CentOS-6-32
 elif [ "$1" = CentOS-7-ICC-beta ]
 then
     if [ -z "$ICC_BETA_ACTIVATION_SERIAL_NUMBER" -a -n "$TRAVIS_PULL_REQUEST" ]; then
@@ -49,45 +62,45 @@ then
     fi
 elif [ "$1" = CentOS-7-ICC ]
 then
-  dockerbuild centos7-icc CentOS-7-ICC
+  dockerbuildandtest centos7-icc CentOS-7-ICC
 elif [ "$1" = CentOS-7 ]
 then
-  dockerbuild centos7 CentOS-7
-  dockerbuild centos7-release CentOS-7-Release
+  dockerbuildandtest centos7 CentOS-7
+  dockerbuildandtest centos7-release CentOS-7-Release
 elif [ "$1" = Debian-stable ]
 then
-  dockerbuild debian-stable Debian-stable
-  dockerbuild debian-stable-release Debian-stable-Release
-  dockerbuild debian-stable-cross-compilation-for-arm Debian-stable-cross-compilation-for-arm
+  dockerbuildandtest debian-stable Debian-stable
+  dockerbuildandtest debian-stable-release Debian-stable-Release
+  dockerbuildandtest debian-stable-cross-compilation-for-arm Debian-stable-cross-compilation-for-arm
 elif [ "$1" = Debian-testing ]
 then
-  dockerbuild debian-testing Debian-testing
-  dockerbuild debian-testing-clang-main Debian-testing-clang-main
+  dockerbuildandtest debian-testing Debian-testing
+  dockerbuildandtest debian-testing-clang-main Debian-testing-clang-main
 elif [ "$1" = Fedora ]
 then
-  dockerbuild fedora Fedora
-  dockerbuild fedora-with-leda Fedora-with-LEDA
-  dockerbuild fedora-release Fedora-Release
-  dockerbuild fedora-strict-ansi Fedora-strict-ansi
+  dockerbuildandtest fedora Fedora
+  dockerbuildandtest fedora-with-leda Fedora-with-LEDA
+  dockerbuildandtest fedora-release Fedora-Release
+  dockerbuildandtest fedora-strict-ansi Fedora-strict-ansi
 elif [ "$1" = Fedora-32 ]
 then
-  dockerbuild fedora-32 Fedora-32
-  dockerbuild fedora-32-release Fedora-32-Release
+  dockerbuildandtest fedora-32 Fedora-32
+  dockerbuildandtest fedora-32-release Fedora-32-Release
 elif [ "$1" = Fedora-rawhide ]
 then
-  dockerbuild fedora-rawhide Fedora-rawhide
-  dockerbuild fedora-rawhide-release Fedora-rawhide-Release
+  dockerbuildandtest fedora-rawhide Fedora-rawhide
+  dockerbuildandtest fedora-rawhide-release Fedora-rawhide-Release
 elif [ "$1" = Ubuntu ]
 then
-  dockerbuild ubuntu Ubuntu
-  dockerbuild ubuntu-cxx11 Ubuntu-CXX11
-  dockerbuild ubuntu-no-deprecated-code Ubuntu-NO_DEPRECATED_CODE
-  dockerbuild ubuntu-no-gmp-no-leda Ubuntu-no-gmp-no-leda
+  dockerbuildandtest ubuntu Ubuntu
+  dockerbuildandtest ubuntu-cxx11 Ubuntu-CXX11
+  dockerbuildandtest ubuntu-no-deprecated-code Ubuntu-NO_DEPRECATED_CODE
+  dockerbuildandtest ubuntu-no-gmp-no-leda Ubuntu-no-gmp-no-leda
 elif [ "$1" = Ubuntu-GCC-master ]
 then
-  dockerbuild ubuntu-gcc6 Ubuntu-GCC6
-  dockerbuild ubuntu-gcc6-cxx1z Ubuntu-GCC6-CXX1Z
-  dockerbuild ubuntu-gcc6-release Ubuntu-GCC6-Release
-  dockerbuild ubuntu-gcc_master_cxx20-release Ubuntu-GCC_master_cpp20-Release
+  dockerbuildandtest ubuntu-gcc6 Ubuntu-GCC6
+  dockerbuildandtest ubuntu-gcc6-cxx1z Ubuntu-GCC6-CXX1Z
+  dockerbuildandtest ubuntu-gcc6-release Ubuntu-GCC6-Release
+  dockerbuildandtest ubuntu-gcc_master_cxx20-release Ubuntu-GCC_master_cpp20-Release
 fi
 docker images
