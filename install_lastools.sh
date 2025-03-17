b9b52ebcda9b7f1ee87b8f9054b416aa01e0419c
Laurent Rineau <laurent.rineau@cgal.org>
2025-03-17 15:49:05 +0100

    Revert "the patch was merged in upstream LAStools"
    This reverts commit a3481686adc219bf15ee731bf9f820af5f7d1e23.

--- install_lastools.sh
+++ install_lastools.sh
@@ -7,6 +7,9 @@ wget https://github.com/LAStools/LAStools/archive/refs/heads/master.zip
 unzip master.zip
 cd LAStools-master
 
+dir=$(dirname $0)
+patch -p1 < "${dir}/LAStools.patch"
+
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
